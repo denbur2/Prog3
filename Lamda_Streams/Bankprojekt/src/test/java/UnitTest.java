b0efb6970b3a03ac24a33199abe6c304bd86fe1f
hendrik armbrecht <schmurgler7@gmail.com>
2025-05-23 21:54:54 +0200

    diverse sachen!

--- Lamda_Streams/Bankprojekt/src/test/java/UnitTest.java
+++ Lamda_Streams/Bankprojekt/src/test/java/UnitTest.java
@@ -1,11 +1,19 @@
-import bankprojekt.verarbeitung.*;
-import bankprojekt.verwaltung.Bank;
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import bankprojekt.verarbeitung.Geldbetrag;
+import bankprojekt.verarbeitung.GesperrtException;
+import bankprojekt.verarbeitung.Kunde;
+import bankprojekt.verarbeitung.UeberweisungsfaehigesKonto;
+import bankprojekt.verwaltung.Bank;
 
 public class UnitTest {
     Bank bank = new Bank(12345678);
@@ -13,7 +21,6 @@ public class UnitTest {
     Kunde kunde2 = new Kunde("Lilly", "Musterfrau", "Musterstraße 2", LocalDate.of(1991, 6, 14));
     long kontoNr1 = bank.girokontoErstellen(kunde);
     long kontoNr2 = bank.girokontoErstellen(kunde2);
-
 
     @Test
     void testGeldUeberweisen() throws GesperrtException {
